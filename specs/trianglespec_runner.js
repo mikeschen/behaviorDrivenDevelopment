eddd002d50ecb3570a25d95ec9ee13131b7fdec3
Brianna Popson and Michael Schennum <mikeschenphoto@gmail.com>
2016-01-19 10:01:29 -0800

    added if else statements

--- specs/trianglespec_runner.js
+++ specs/trianglespec_runner.js
@@ -2,4 +2,16 @@ describe('triangle', function() {
   it("is false when sum of 2 sides is <= the 3rd side", function() {
     expect(triangle(2, 2, 4)).to.equal(false);
   });
+
+  it("is equilateral when all sides are equal", function() {
+    expect(triangle(2, 2, 2)).to.equal("equilateral");
+  });
+
+  it("is isoceles when two of the three sides are equal", function() {
+    expect(triangle(2, 2, 3)).to.equal("isoceles");
+  });
+
+  it("is scalene when no sides are equal", function() {
+    expect(triangle(2, 3, 4)).to.equal("scalene");
+  });
 });
